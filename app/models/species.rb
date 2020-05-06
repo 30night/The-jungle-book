c505f7edbbbb19ce36f151871cb9a91388502049
Emma Bindi <emma.bindi@outlook.com>
2020-05-07 09:27:39 +1000

    trying to setup species list with initializer method, no luck yet all commented out atm

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,7 @@
 class Species < ApplicationRecord
   has_many :animals, dependent: :destroy
+
+  # def species_type_list
+  #   species_type = 
+  # end
 end
---
+++ config/initializers/species_type.rb
@@ -0,0 +1,13 @@
+# class Species_Type
+#   def.self.type
+#     # species_type = [
+#       ["Insect"], 
+#       ["Reptile"], 
+#       ["Bird"],
+#       ["Fish"], 
+#       ["Mammal"], 
+#       ["Mammal (Primate)"], 
+#       ["Amphibian"], 
+#       ["Mollusc"]
+#   end
+# end
