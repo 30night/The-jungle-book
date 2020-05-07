07da3c4f7a46f73d8d21654f30147a609bb40b44
Emma Bindi <emma.bindi@outlook.com>
2020-05-07 17:01:33 +1000

    ran precompile asset command and it changed some fiiles, we also fixed empty css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   #to use my helper file: 
-  config.action_controller.include_all_helpers = true
+  # config.action_controller.include_all_helpers = true
 
 end
